0a65eb4ba77f40f60f3bf5af056e5cef3170cf17
Abhishek Goyal <abhishek252167@gmail.com>
2020-07-05 08:23:01 +0530

    removed ouput files

--- LeetCode/Implement_strStr().cpp
+++
@@ -1,9 +0,0 @@
-// https://leetcode.com/problems/implement-strstr/
-// Accepted    4 ms
-
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        return haystack.find(needle);
-    }
-};
